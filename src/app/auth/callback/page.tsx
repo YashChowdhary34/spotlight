450fab5d7b1b9afac0b497a8bf059bec1a21b9fa
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-05-26 10:14:25 +0530

    userauth server action & callback

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,19 +1,19 @@
-// import { onAuthenticateUser } from "@/actions/user";
-// import { redirect } from "next/navigation";
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-// export default async function AuthCallbackPage() {
-//   // Authentication
-//   const auth = await onAuthenticateUser();
+export default async function AuthCallbackPage() {
+  // Authentication
+  const auth = await onAuthenticateUser();
 
-//   console.log("Auth response:", auth);
+  console.log("Auth response:", auth);
 
-//   if (auth.status === 200 || auth.status === 201) {
-//     if (auth.user?.workspace && auth.user.workspace[0]?.id) {
-//       redirect(`/dashboard/${auth.user.workspace[0].id}`);
-//     } else {
-//       redirect("/auth/sign-in");
-//     }
-//   } else {
-//     redirect("/auth/sign-in");
-//   }
-// }
+  if (auth.status === 200 || auth.status === 201) {
+    if (auth.user?.workspace && auth.user.workspace.id) {
+      redirect(`/dashboard/${auth.user.workspace.id}`);
+    } else {
+      redirect("/auth/sign-in");
+    }
+  } else {
+    redirect("/auth/sign-in");
+  }
+}
