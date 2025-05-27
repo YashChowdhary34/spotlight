81c13505d24f3dff84a6c19f1059ea350be8c3e1
yashchowdhary34 <yashchowdhary34@gmail.com>
2025-05-27 19:04:30 +0530

    fixed minor bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ export default async function DashboardPage() {
   //authentication
   const auth = await onAuthenticateUser();
   if (auth.status === 200 || auth.status === 201) {
-    return redirect(`/dashboard/${auth.user?.workspace.id}`);
+    return redirect(`/dashboard/${auth.user?.workspace?.id}`);
   }
   if (auth.status === 400 || auth.status === 500 || auth.status === 404) {
     return redirect("/auth/sign-in");
